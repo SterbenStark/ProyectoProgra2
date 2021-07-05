1b45392019742e623db0ae856d627de4ab456a36
Milton José Gómez Pérez <80368138+SterbenStark@users.noreply.github.com>
2021-07-04 22:56:49 -0600

    a
    a

--- Publisher.php
+++ Publisher.php
@@ -5,15 +5,15 @@ class Publisher extends Model {
 
   static $publisher = [
     ['id'=>1,'publisher'=>'John Walker','country'=>'Estados Unidos',
-    'founded'=>'2009','status'=>'Activo','books__book_id'=>'1',
+    'founded'=>'1980','status'=>'Activo','books__book_id'=>'1',
     'books__title'=>'Reglamentos', ],  
 
     ['id'=>2,'publisher'=>'Shiva','country'=>'India',
     'founded'=>'1844','status'=>'Activo','books__book_id'=>'3',
     'books__title'=>'Boletines Informativos', ], 
 
-    ['id'=>3,'publisher'=>'Jack el Destripador','country'=>'United Kingdom',
-    'founded'=>'1844','status'=>'Activo','books__book_id'=>'2',
+    ['id'=>3,'publisher'=>'Jack el Destripador','country'=>'Reino Unido',
+    'founded'=>'1888','status'=>'Activo','books__book_id'=>'2',
     'books__title'=>'Politicas de COVID-19', ], 
   ];
 
