f903319151e340b536602de21dd1923f80b16469
Milton José Gómez Pérez <80368138+SterbenStark@users.noreply.github.com>
2021-07-04 22:37:47 -0600

    a
    a

--- Book.php
+++ Book.php
@@ -5,19 +5,19 @@ class Book extends Model {
 
   static $books = [
     ['id'=>1,'title'=>'Reglamentos ','copyright'=>'2010',
-    'language'=>'Ingles/Español','pages'=>'15','author'=>'Abraham Silberschatz','author_id'=>'1',
+    'language'=>'Ingles/Español','pages'=>'15','author'=>'Milton Gómez Pérez','author_id'=>'1',
     'publisher'=>'John Wiley & Sons','publisher_id'=>'1',],
 
     ['id'=>2,'title'=>'Politicas de COVID-19','copyright'=>'2020',
-     'language'=>'Ingles/Español','pages'=>'8','author'=>'Abraham Silberschatz','author_id'=>'1',
+     'language'=>'Ingles/Español','pages'=>'8','author'=>'Jiandro Sibaja Granados','author_id'=>'1',
      'publisher'=>'John Wiley & Sons','publisher_id'=>'1',], 
 
      ['id'=>3,'title'=>'Boletines Informativos','copyright'=>'2015',
-      'language'=>'Ingles/Español','pages'=>'80','author'=>'Andrew S. Tanenbaum','author_id'=>'2',
+      'language'=>'Ingles/Español','pages'=>'80','author'=>'Cristina Vasquez Granados','author_id'=>'2',
       'publisher'=>'Pearson Education','publisher_id'=>'2',],  
 
       ['id'=>4,'title'=>'Normativa Interna','copyright'=>'2010',
-       'language'=>'Español','pages'=>'90','author'=>'Andrew S. Tanenbaum','author_id'=>'2',
+       'language'=>'Español','pages'=>'90','author'=>'Milton Gómez Pérez','author_id'=>'2',
        'publisher'=>'Pearson Education','publisher_id'=>'2',],  
   ];
 
